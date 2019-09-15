cc1a8f7f36e6a2aa420c4a4fe4b41886635ed149
Marius Bongarts <mb709027@fh-muenster.de>
2019-09-15 22:30:34 +0200

    Implement PUT, DELETE rest points for mark

--- NestJsServer/src/marks/marks.controller.ts
+++ NestJsServer/src/marks/marks.controller.ts
@@ -3,7 +3,7 @@ import { UsersService } from './../users/users.service';
 import { Mark } from './mark.interface';
 import { MarksService } from './marks.service';
 import { UserJwt } from './../users/decorators/email.decorator';
-import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Delete, Param, Put } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('marks')
@@ -27,10 +27,29 @@ export class MarksController {
     return marks;
   }
 
+  @Get(':id')
+  @UseGuards(AuthGuard())
+  async getMarkById(@UserJwt() userJwt, @Param('id') markId) {
+    return await this.marksService.findMarkById(userJwt as JwtPayload, markId);
+  }
+
   @Post('')
   @UseGuards(AuthGuard())
   async createMark(@UserJwt() userJwt, @Body() mark) {
     const createdMark = await this.marksService.createMark(userJwt as JwtPayload, mark);
     return createdMark;
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard())
+  async deleteMark(@UserJwt() userJwt, @Param('id') markId) {
+    const createdMark = await this.marksService.deleteMark(userJwt as JwtPayload, markId);
+    return createdMark;
+  }
+
+  @Put('')
+  @UseGuards(AuthGuard())
+  async updateMark(@UserJwt() userJwt, @Body() mark) {
+    return await this.marksService.updateMark(userJwt as JwtPayload, mark);
   }
 }
--- NestJsServer/src/marks/marks.service.ts
+++ NestJsServer/src/marks/marks.service.ts
@@ -7,15 +7,27 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class MarksService {
-  constructor(@InjectModel('Mark') private markModel: Model<Mark>) {}
+  constructor(@InjectModel('Mark') private markModel: Model<Mark>) { }
 
   async getMarksForUser(user: JwtPayload) {
-    return await this.markModel.find({_user: user._id}).exec();
+    return await this.markModel.find({ _user: user._id }).exec();
+  }
+
+  async findMarkById(user: JwtPayload, markId: string) {
+    return await this.markModel.findOne({ _user: user._id, _id: markId });
   }
 
   async createMark(user: JwtPayload, mark: Mark) {
     const createdMark = new this.markModel(mark);
     createdMark._user = user._id;
     return await createdMark.save();
+  }
+
+  async deleteMark(user: JwtPayload, markId: string) {
+    return await this.markModel.deleteOne({ _user: user._id, _id: markId });
+  }
+
+  async updateMark(user: JwtPayload, mark: Mark) {
+    return await this.markModel.updateOne({ _user: user._id, _id: mark._id }, mark);
   }
 }
